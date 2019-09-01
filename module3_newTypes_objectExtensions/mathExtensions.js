daa3e781acf94310f1500f717de93a3c445b5c14
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2019-09-01 10:23:11 +0530

    ES6 Math Extension

---
+++ module3_newTypes_objectExtensions/mathExtensions.js
@@ -0,0 +1,11 @@
+// console.log(Math.sign(0));  // 0
+// console.log(Math.sign(-0)); // -0
+// console.log(Math.sign(20)); // 1
+// console.log(Math.sign(-20)); // -1
+// console.log(Math.sign(NaN)); // NaN
+
+// console.log(Math.cbrt(64)) // 4
+
+console.log(Math.trunc(27.1)); // 27
+console.log(Math.trunc(27.9)); // 27
+console.log(Math.trunc(-27.9)); // -27
